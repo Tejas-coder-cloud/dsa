08bf2d910cf6b6fd16ce9fa4253f7aad21da4f42
Tejas07 <dhatraktejas07@gmail.com>
2025-10-22 15:54:27 +0530

    added the code for lowerbound

--- binary search/upperbound.cpp
+++ binary search/upperbound.cpp
@@ -5,24 +5,25 @@ int upperBound(const vector<int> &nums, int x)
 {
     int start = 0;
     int end = nums.size();
-    while (start <end)
+    while (start < end)
     {
         int mid = start + (end - start) / 2;
         if (nums[mid] > x)
         {
-            end=mid;
+            end = mid;
         }
         else
         {
-            start = mid+1;
+            start = mid + 1;
         }
     }
     return start;
 }
 int main()
-{ vector<int> a={1,2,3,4};
-   int target=2;
-   int ans=upperBound(a,target);
-   cout<<ans;
+{
+    vector<int> a = {1, 2, 3, 4};
+    int target = 2;
+    int ans = upperBound(a, target);
+    cout << ans;
     return 0;
 }
