c951e73255ec1e3adef9a57d402102372346a19b
Tejas07 <dhatraktejas07@gmail.com>
2025-11-22 10:26:10 +0530

    Added the code for strings leetcode question

---
+++ leetcode practice/isSubsequence.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == s.length();
+    }
+};
+int main()
+{
+    Solution sol;
+    cout<<"S is a ssequence of T: "<<sol.isSubsequence("axhc","abcdef");
+    return 0;
+}
---
+++ leetcode practice/valid_palindrome.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool  isAlpnum(char ch)
+    {
+        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z'))
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int start=0, end=s.length()-1;
+        while(start<end)
+        {
+            if(!isAlpnum(s[start]))
+            {
+                start++;
+                continue;
+            }
+              if(!isAlpnum(s[end]))
+            {
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end]))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
+int  main()
+{
+    Solution sol;
+    cout<<"String is palindrome: "<< sol.isPalindrome("Naman");
+    return 0;
+}
