73022e108e70c0559c8dd9099be7553e319db911
Tejas07 <dhatraktejas07@gmail.com>
2025-12-21 07:58:04 +0530

    added the code for linked list

---
+++ linked_list/singly_linked_list.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node(int data)
+    {
+     this->data=data;
+     this->next=NULL;
+    }
+    ~Node()
+    {
+        int value=this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            this->next=NULL;
+        }
+    }
+    
+};
+void insertathead(Node* &head, int data)
+    {
+        Node*temp=new Node(data);
+        temp->next=head;
+        head=temp;
+    }
+    void insertattail(Node* &tail,int data)
+    {
+        Node* temp=new Node(data);
+        tail->next=temp;
+        tail=temp;
+    }
+    void insertatposition(Node* &head,Node* &tail,int position,int data)
+    {
+        if(position==1)
+        {
+            insertathead(head,data);
+            return;
+        }
+        Node* temp=head;
+        int cnt=1;
+        while(cnt<position-1)
+        {
+            temp=temp->next;
+            cnt++;
+        }
+        if(temp->next==NULL)
+        {
+            insertattail(tail,data);
+            return;
+        }
+        Node* nodetoinsert=new Node(data);
+        nodetoinsert->next=temp->next;
+        temp->next=nodetoinsert;
+    }
+    void deletenode(int position, Node * &head)
+    {
+        if(position==1)
+        {
+            Node* temp=head;
+            head=head->next;
+            temp->next=NULL;
+            delete temp;
+        }
+        else
+        {
+            Node* curr=head;
+            Node* prev=NULL;
+            int cnt=1;
+            while(cnt<position)
+            {
+                prev=curr;
+                curr=curr->next;
+                cnt++;
+            }
+            prev->next=curr->next;
+            curr->next=NULL;
+            delete curr;
+        }
+    }
+    void display(Node* &head)
+    {
+        Node*temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+int main()
+{
+    Node* node1=new Node(10);
+    Node* head=node1;
+    Node*tail=node1;
+    insertathead(head,20);
+    insertattail(tail,30);
+    insertatposition(head,tail,2,25);
+    display(head);
+    return 0;
+}
