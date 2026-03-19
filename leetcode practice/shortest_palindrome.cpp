62b6f321ec529a828ae5112748eb56128c58e3d1
Tejas07 <dhatraktejas07@gmail.com>
2026-03-19 06:57:43 +0530

    Modified the code for shortest palindromic string

--- leetcode practice/shortest_palindrome.cpp
+++ leetcode practice/shortest_palindrome.cpp
@@ -1,47 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-class Solution {
-public:
-    std::string shortestPalindrome(std::string s) {
-        std::string rev_s = s;
-        std::reverse(rev_s.begin(), rev_s.end());
-        
-        // Create the combined string with a separator
-        std::string temp = s + "#" + rev_s;
-        
-        int n = temp.length();
-        std::vector<int> lps(n, 0);
-        
-        // Build the LPS array
-        for (int i = 1; i < n; i++) {
-            int j = lps[i-1];
-            
-            while (j > 0 && temp[i] != temp[j]) {
-                j = lps[j-1];
-            }
-            
-            if (temp[i] == temp[j]) {
-                j++;
-            }
-            
-            lps[i] = j;
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> computelps(string s)
+{
+    int len=s.length();
+    vector<int> lps(len,0);
+    for(int i=1;i<len;i++)
+    {
+        int j=lps[i-1];
+        while(j>0 && s[i]!=s[j])
+        {
+            j=lps[j-1];
         }
-        
-        // lps[n-1] is the length of the longest palindromic prefix
-        int match_len = lps[n-1];
-        
-        // Taking the suffix that needs to be mirrored
-        std::string suffix = s.substr(match_len);
-        std::reverse(suffix.begin(), suffix.end());
-        
-        return suffix + s;
+        if(s[i]==s[j])
+        {
+            j++;
+        }
+        lps[i]=j;
     }
-};
+    return lps;
+}
+string shortest_palindrome(string s)
+{
+    int n=s.length();
+    string rev=s;
+    reverse(rev.begin(),rev.end());
+    string temp=s+'#'+rev;
+    vector<int> lps = computelps(temp);
+    int len=lps.back();
+    string suffix=s.substr(len);
+    reverse(suffix.begin(),suffix.end());
+    return suffix+s;
+}
 int main()
 {
-    Solution sol;
-    std::cout<<sol.shortestPalindrome("There");
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    cout<<"Shortest palindromic string is as follows: "<<shortest_palindrome(s);
     return 0;
 }
