feebca273d4ee24ed9ca902a2778aad1937450b6
Tejas07 <dhatraktejas07@gmail.com>
2026-03-20 14:26:39 +0530

    Modified the code for concatenation of 2 substrings

--- leetcode practice/substring_with_concatenation_of_words.cpp
+++ leetcode practice/substring_with_concatenation_of_words.cpp
@@ -1,47 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
-    vector<int> findSubstring(string s, vector<string>& words) {
+    vector<int> findSubstring(string s, vector<string>& words) 
+    {
         vector<int> result;
-        if (s.empty() || words.empty()) return result;
-
+        if (s.empty() || words.empty())
+        {
+        return result;
+        }
         int n = s.length();
         int numWords = words.size();
         int wordLen = words[0].length();
         int totalLen = numWords * wordLen;
-
-        // 1. Build the frequency map for the required words
         unordered_map<string, int> counts;
-        for (const string& w : words) counts[w]++;
-
-        // 2. Iterate through all possible starting offsets (0 to wordLen - 1)
-        for (int i = 0; i < wordLen; ++i) {
+        for (string & w : words)
+        { 
+            counts[w]++;
+        }
+        for (int i = 0; i < wordLen; ++i) 
+        {
             int left = i, count = 0;
             unordered_map<string, int> window;
-
-            // 3. Slide the window across the string
-            for (int j = i; j <= n - wordLen; j += wordLen) {
+            for (int j = i; j <= n - wordLen; j += wordLen) 
+            {
                 string word = s.substr(j, wordLen);
                 
-                if (counts.count(word)) {
+                if (counts.count(word)) 
+                {
                     window[word]++;
                     count++;
-                    
-                    // If we have too many of 'word', slide 'left' to the right
-                    while (window[word] > counts[word]) {
+                    while (window[word] > counts[word]) 
+                    {
                         string leftWord = s.substr(left, wordLen);
                         window[leftWord]--;
                         count--;
                         left += wordLen;
                     }
-                    
-                    // 4. If window matches total length, we found a concatenation
-                    if (count == numWords) {
+                    if (count == numWords) 
+                    {
                         result.push_back(left);
                     }
-                } else {
-                    // Not a valid word, reset the window
+                } 
+                else 
+                {
                     window.clear();
                     count = 0;
                     left = j + wordLen;
@@ -54,8 +57,8 @@ public:
 int main() 
 {
     Solution sol;
-    string s="";
-    vector<string> word;
+    string s="Tejas";
+    vector<string> word={"Te","ja"};
     vector<int> result=sol.findSubstring(s, word);
     for(int j:result)
     {
