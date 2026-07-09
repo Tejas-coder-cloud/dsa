e94a23827ee402ae741572ff1e7e55948e2115dc
Tejas-coder-cloud <dhatraktejas07@gmail.com>
2026-07-09 12:31:59 +0530

    Added the memoized version of frog_jump

--- dynamic programming/frog_jump.cpp
+++ dynamic programming/frog_jump.cpp
@@ -29,3 +29,33 @@ int main()
     cout<<"Minimum cost is: "<<frog_jump(arr); 
     return 0;
 }
+/*
+Memoized version of the code 
+Time complexity:O(N)
+Space complexity:O(N)
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int i , vector<int> &arr,vector<int> & dp)
+{
+    if(i==0) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int fs=solve(i-1,arr,dp)+abs(arr[i]-arr[i-1]);
+    int ss=INT_MAX;
+    if(i>1)  ss=solve(i-2,arr,dp)+abs(arr[i]-arr[i-2]);
+    dp[i]=min(fs,ss);
+    return dp[i];
+}
+int frog_jump(vector<int> &arr)
+{
+    int n=arr.size();
+    vector<int> dp(n,-1);
+    return solve(n-1,arr,dp);
+}
+int main() 
+{
+    vector<int> arr={10,20,10};
+    int ans=frog_jump(arr);
+    cout<<"The minimum energy required is: "<<ans<<endl;
+    return 0;
+}
+*/
