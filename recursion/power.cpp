5aebff6f2365c66e6bd22b060dfe294f7ec66fc1
Tejas-coder-cloud <dhatraktejas07@gmail.com>
2026-07-10 12:38:27 +0530

    Added the code for power(x,n)

--- recursion/power.cpp
+++ recursion/power.cpp
@@ -1,45 +1,44 @@
-#include<bits/stdc++.h>
+/*
+Power of a number
+Time complexity:O(logn)
+Space complexity:O(logn)
+*/
+#include <bits/stdc++.h>
 using namespace std;
-int power(int a ,int n)
+double power(double x, long long n)
 {
-    if(n==0)
+    if (n == 0)
+        return 1.0;
+
+    double half = power(x, n / 2);
+
+    if (n % 2 == 0)
+        return half * half;
+    else
+        return x * half * half;
+}
+
+double myPow(double x, int n)
+{
+    long long N = n; // Convert int to long long
+
+    if (N < 0)
     {
-        return 1; 
+        x = 1.0 / x;
+        N = -N;
     }
-    return a*power(a,n-1);
+
+    return power(x, N);
 }
 int main()
 {
-    int a,n;
-    cout<<"Enter the number:";
-    cin>>a;
-    cout<<"Enter the power of the number: ";
+    int x,n;
+    cout<<"Enter the value of x: ";
+    cin>>x;
+    cout<<"Enter the value of n: ";
     cin>>n;
-    cout<<n<<" "<<"Power of "<<a<<" "<<" is "<<power(a,n);
+    double ans=myPow(x,n);
+    cout<<"The value of "<<x<<" raised to the power of "<<n<<" is "<<ans<<endl;
     return 0;
 }
-// #include<iostream>
-// using namespace std;
-// int power(int a, int n) {
-//     int result = 1;
-//     while (n > 0) {
-//         if (n % 2 == 1) {
-//             result *= a;
-//         }
-//         a *= a;
-//         n /= 2;
-//     }
-//     return result;
-// }
-// int main()
-// {
-//     int a,n;
-//     cout<<"Enter the number:";
-//     cin>>a;
-//     cout<<"Enter the power of the number: ";
-//     cin>>n;
-//     cout<<n<<" "<<"Power of "<<a<<" "<<" is "<<power(a,n);
-//     return 0;
 
-// }
-// A more efficient approach using iterative method 
